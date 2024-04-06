0d80a9216a8ce07f3b287dfd3519bfb3c43163f7
karimadnan <karimmadnan@gmail.com>
2024-04-06 21:36:39 +0200

    add: query client to lib

--- app/exchange-calculator/components/exchange-form.tsx
+++ app/exchange-calculator/components/exchange-form.tsx
@@ -58,7 +58,7 @@ export default function({ listQuotes }: ExchangeFormProps) {
             }, CONVERT_DEBOUNCE_MILLISECONDS)
         }
     }
-    
+
     return (
         <Skeleton 
             amount={
---
+++ lib/queryClient.ts
@@ -0,0 +1,16 @@
+function makeQueryClient() {
+    const fetchMap = new Map<string, Promise<any>>();
+    return function queryClient<QueryResult>(
+      name: string,
+      query: () => Promise<QueryResult>
+    ): Promise<QueryResult> {
+      if (!fetchMap.has(name)) {
+        fetchMap.set(name, query());
+      }
+      return fetchMap.get(name)!;
+    };
+  }
+  
+  const queryClient = makeQueryClient();
+
+  export default queryClient
