d048a30af54ea6adc4b3643a8fc0e64abebf8cef
karimadnan <karimmadnan@gmail.com>
2024-04-06 02:40:07 +0200

    chore: ui lib entry point

---
+++ lib/ui/index.ts
@@ -0,0 +1,5 @@
+export * from './select'
+export { default as BackgroundImage } from './background-image'
+export { default as Label } from './label'
+export { default as Input } from './input'
+export { default as Button } from './button'
