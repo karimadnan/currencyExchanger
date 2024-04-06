d84340dad9f64aed7c560ea3a83abc2a969be603
karimadnan <karimmadnan@gmail.com>
2024-04-06 19:56:33 +0200

    add: currency select component

---
+++ app/exchange-calculator/components/currency-select.tsx
@@ -0,0 +1,40 @@
+import { 
+    Select, 
+    SelectContent, 
+    SelectGroup, 
+    SelectItem, 
+    SelectTrigger, 
+    SelectValue 
+} from "@currency-exchanger/lib/ui";
+
+interface CurrencySelectProps extends React.ComponentProps<typeof Select> {
+    disabledValue: string
+    options: string[]
+}
+
+export default function({
+    options,
+    disabledValue,
+    ...rest
+}: CurrencySelectProps) {
+    return (
+        <Select {...rest}>
+            <SelectTrigger>
+                <SelectValue placeholder="Currency" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    {options.map((record, index) => 
+                        <SelectItem 
+                            key={`${record}-${index}`} 
+                            value={record} 
+                            disabled={record === disabledValue}
+                        >
+                            {record}
+                        </SelectItem>
+                    )}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    )
+}
