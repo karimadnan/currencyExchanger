745ffe54bd21a86ca3d942e065613989a3961a16
karimadnan <karimmadnan@gmail.com>
2024-04-06 20:12:13 +0200

    chore: remove skeleton loader for string conversion from initial skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,16 +5,11 @@ export default function() {
     return (
         <div className="h-screen w-screen">
             <ExchangeSkeleton
-                amount={<SkeletonLoader className="h-4 w-full" />}
-                selectFrom={<SkeletonLoader className="h-4 w-full" />} 
-                selectTo={<SkeletonLoader className="h-4 w-full" />}
+                amount={<SkeletonLoader className="h-10 w-full" />}
+                selectFrom={<SkeletonLoader className="h-10 w-full" />} 
+                selectTo={<SkeletonLoader className="h-10 w-full" />}
                 swapButton={<SkeletonLoader className="h-12 w-12 rounded-full" />}
-                footer={
-                    <>
-                        <SkeletonLoader className="h-8 rounded-full w-full mb-4" />
-                        <SkeletonLoader className="h-4 w-full" />
-                    </>
-                }
+                footer={<SkeletonLoader className="h-8 rounded-full w-full mb-4" />}
             />
         </div>
     )
