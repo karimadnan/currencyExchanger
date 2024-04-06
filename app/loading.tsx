d49d4f4389f925dccda3a0eb4525432cc71ecb5d
karimadnan <karimmadnan@gmail.com>
2024-04-06 19:57:29 +0200

    add: loading skeleton for exchange component

---
+++ app/loading.tsx
@@ -0,0 +1,21 @@
+import { SkeletonLoader } from "@currency-exchanger/lib/ui";
+import { ExchangeSkeleton } from "./exchange-calculator/components";
+
+export default function() {
+    return (
+        <div className="h-screen w-screen">
+            <ExchangeSkeleton
+                amount={<SkeletonLoader className="h-4 w-full" />}
+                selectFrom={<SkeletonLoader className="h-4 w-full" />} 
+                selectTo={<SkeletonLoader className="h-4 w-full" />}
+                swapButton={<SkeletonLoader className="h-12 w-12 rounded-full" />}
+                footer={
+                    <>
+                        <SkeletonLoader className="h-8 rounded-full w-full mb-4" />
+                        <SkeletonLoader className="h-4 w-full" />
+                    </>
+                }
+            />
+        </div>
+    )
+}
